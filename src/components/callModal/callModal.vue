db2b7b6b29a2281569588ed70e34ee93d6b6f42f
Leon1Zhu <zl937154020@163.com>
2018-11-06 23:51:33 +0800

    详情页底部修改和电话预约弹出页修改

--- src/components/callModal/callModal.vue
+++ src/components/callModal/callModal.vue
@@ -1,23 +1,29 @@
-
 <template>
   <transition name="modal">
     <div class="modal-mask">
       <div class="modal-wrapper">
         <div class="modal-container">
-            <!--
-          <div class="modal-header">
-            <slot name="header">
-              请输入电话号码
-            </slot>
-          </div>
+          <!--
+        <div class="modal-header">
+          <slot name="header">
+            请输入电话号码
+          </slot>
+        </div>
 -->
           <div class="modal-body">
-            <input type="text" placeholder="  请输入电话号码" v-model="call" @input="handleFirstNameUpdate()" />
+            <div class="mu-form-item mu-form-item__has-label">
+              <div class="mu-form-item-label" style="width: 100px;">Input</div><!---->
+              <div class="mu-form-item-content">
+                <mu-text-field v-model="form.input"></mu-text-field>
+              </div>
+            </div>
+            <input type="text" placeholder="  请输入电话号码" v-model="call" @input="handleFirstNameUpdate()"/>
           </div>
 
+
           <div class="modal-footer">
-              <mu-button color="#ffffff" @click="cancel">取消</mu-button>
-              <mu-button color="#0284DC" @click="save">保存</mu-button>
+            <mu-button color="#ffffff" @click="cancel">取消</mu-button>
+            <mu-button color="#0284DC" @click="save">保存</mu-button>
           </div>
         </div>
       </div>
@@ -26,27 +32,27 @@
 </template>
 
 <script>
-import './callModal.scss';
+  import './callModal.scss';
 
-export default {
-  name: 'call-modal',
-  data() {
-    return {
+  export default {
+    name: 'call-modal',
+    data() {
+      return {
         call: null,
-    };
-  },
-  components: {
-  },
-  created() {},
-  mounted() {
-  },
-  methods: {
-      cancel () {
-          this.$emit('close');
+      };
+    },
+    components: {},
+    created() {
+    },
+    mounted() {
+    },
+    methods: {
+      cancel() {
+        this.$emit('close');
       },
-      save () {
-          this.$emit('close');
+      save() {
+        this.$emit('close');
       }
-  },
-};
+    },
+  };
 </script>

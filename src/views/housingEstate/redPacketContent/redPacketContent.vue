af0f5ada7cabb730175fa761e2afe411190c5174
Leon1Zhu <zl937154020@163.com>
2018-12-17 19:26:47 +0800

    红包页面

---
+++ src/views/housingEstate/redPacketContent/redPacketContent.vue
@@ -0,0 +1,24 @@
+ <!--
+  @author zhuliang
+  @date 2018/12/17 7:24 PM
+-->
+<template>
+<section class="redPacketContent">
+
+</section>
+</template>
+
+<script>
+import './redPacketContent.scss';
+
+export default {
+  name: 'red-packet-content',
+  data() {
+    return {};
+  },
+  components: {},
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
